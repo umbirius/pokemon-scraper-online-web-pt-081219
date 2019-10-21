3ccb8617dbb0f81416aae7a73327ae827097268e
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-21 19:07:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,18 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
   
+<<<<<<< HEAD
   def initialize(id:, name:, type:, db:)
+=======
+  def initialize(id: nil, name:, type:, db:)
+>>>>>>> 2ddcf72e5bdb07509dab0aef17b919ebed8ed592
     @id = id
     @name = name
     @type = type
     @db = db
   end 
   
+<<<<<<< HEAD
   def self.save(name, type, db) 
     sql =<<-SQL
       INSERT INTO pokemon (name, type)
@@ -29,4 +34,7 @@ class Pokemon
     Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db: db)
   end 
   
+=======
+
+>>>>>>> 2ddcf72e5bdb07509dab0aef17b919ebed8ed592
 end
